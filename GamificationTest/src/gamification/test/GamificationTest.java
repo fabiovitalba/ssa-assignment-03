8c947eeba310cb4165309a3596cf80f46bd0ce09
Fabio.Vitalba <fabvit9@gmail.com>
2024-04-14 13:20:58 +0200

    Finished implementation of GamificationTest basic test

--- GamificationTest/src/gamification/test/GamificationTest.java
+++ GamificationTest/src/gamification/test/GamificationTest.java
@@ -1,5 +1,6 @@
 package gamification.test;
 
+import gamification.api.FailedExecutionException;
 import gamification.api.GameRule;
 import gamification.api.GamificationFacade;
 import gamification.rules.RecordPoints;
@@ -8,9 +9,11 @@ import gamification.user.User;
 import gamification.user.UserRegistry;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GamificationTest {
     @Test
-    void basicFrameworkUsage() {
+    void basicFrameworkUsage() throws FailedExecutionException {
         // Configure the user
         User user = new User("Tester");
         UserRegistry.setCurrentUser(user);
@@ -20,5 +23,13 @@ public class GamificationTest {
         // We assign the Game Rule to DummyTask.class
         // In the setGameRule class, we specified that we can only assign rules to Task-implementing classes
         GamificationFacade.getInstance().setGameRule(gameRule, DummyTask.class);
+
+        // Execute the task
+        GamificationFacade.getInstance().execute(new DummyTask());
+        GamificationFacade.getInstance().execute(new DummyTask());
+
+        // Verify the points
+        assertEquals(20, user.getPoints());
+        assertEquals(10, user.getLastPointsGot());
     }
 }

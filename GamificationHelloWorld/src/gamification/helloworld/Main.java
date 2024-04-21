97d2519cb7f3855ddef719ecfd9c148669d87930
Fabio.Vitalba <fabvit9@gmail.com>
2024-04-21 10:21:34 +0200

    Added GamificationHelloWorld example module

---
+++ GamificationHelloWorld/src/gamification/helloworld/Main.java
@@ -0,0 +1,7 @@
+package gamification.helloworld;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ GamificationHelloWorld/src/module-info.java
@@ -0,0 +1,3 @@
+module GamificationHelloWorld {
+	requires GamificationFramework;
+}
